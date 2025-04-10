9dc2fd78f551bb83f7c2ee901cc184d5019f3719
Francisco Sousa <franciscoeds1987@gmail.com>
2025-04-10 17:26:04 -0300

    feat: criar interface ConsentRepository para persistência com MongoDB

---
+++ src/main/java/com/sensedia/sample/consents/repository/ConsentRepository.java
@@ -0,0 +1,11 @@
+package com.sensedia.sample.consents.repository;
+
+import com.sensedia.sample.consents.domain.Consent;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ConsentRepository extends MongoRepository<Consent, UUID> {
+}
