e94078aafee1aeefb4f4950d9ff927589db07d37
Francisco Sousa <franciscoeds1987@gmail.com>
2025-04-10 21:54:59 -0300

    feat: implementar endpoints REST usando contrato IConsentApi

--- src/main/java/com/sensedia/sample/consents/rest/ConsentApi.java
+++ src/main/java/com/sensedia/sample/consents/rest/ConsentApi.java
@@ -1,18 +1,49 @@
 package com.sensedia.sample.consents.rest;
 
+import com.sensedia.sample.consents.dto.ConsentRequestDTO;
+import com.sensedia.sample.consents.dto.ConsentResponseDTO;
+import com.sensedia.sample.consents.service.ConsentService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
 @Slf4j
 public class ConsentApi implements IConsentApi {
 
+	private final ConsentService service;
+
 	@Override
-	public ResponseEntity<Object> findAll() {
-		log.info("Requisição recebida!!!!!!");
-		return ResponseEntity.badRequest().body("Dummy");
+	public ResponseEntity<ConsentResponseDTO> createConsent(ConsentRequestDTO dto) {
+		return ResponseEntity.ok(service.createConsent(dto));
+	}
+
+	@Override
+	public ResponseEntity<List<ConsentResponseDTO>> getAllConsents() {
+		return ResponseEntity.ok(service.getAllConsents());
+	}
+
+	@Override
+	public ResponseEntity<ConsentResponseDTO> getConsentById(UUID id) {
+		return ResponseEntity.ok(service.getConsentById(id));
+	}
+
+	@Override
+	public ResponseEntity<ConsentResponseDTO> updateConsent(UUID id, ConsentRequestDTO dto) {
+		return ResponseEntity.ok(service.updateConsent(id, dto));
+	}
+
+	@Override
+	public ResponseEntity<Void> deleteConsent(UUID id) {
+		service.deleteConsent(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- src/main/java/com/sensedia/sample/consents/rest/IConsentApi.java
+++ src/main/java/com/sensedia/sample/consents/rest/IConsentApi.java
@@ -1,10 +1,30 @@
 package com.sensedia.sample.consents.rest;
 
+import com.sensedia.sample.consents.dto.ConsentRequestDTO;
+import com.sensedia.sample.consents.dto.ConsentResponseDTO;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.UUID;
+
+@RequestMapping("/consents")
 public interface IConsentApi {
 
-	@GetMapping(value = "/consents", produces = { "application/json" })
-	ResponseEntity<Object> findAll();
+	@PostMapping
+	ResponseEntity<ConsentResponseDTO> createConsent(@RequestBody @Valid ConsentRequestDTO dto);
+
+	@GetMapping
+	ResponseEntity<List<ConsentResponseDTO>> getAllConsents();
+
+	@GetMapping("/{id}")
+	ResponseEntity<ConsentResponseDTO> getConsentById(@PathVariable UUID id);
+
+	@PutMapping("/{id}")
+	ResponseEntity<ConsentResponseDTO> updateConsent(@PathVariable UUID id, @RequestBody @Valid ConsentRequestDTO dto);
+
+	@DeleteMapping("/{id}")
+	ResponseEntity<Void> deleteConsent(@PathVariable UUID id);
+
 }
