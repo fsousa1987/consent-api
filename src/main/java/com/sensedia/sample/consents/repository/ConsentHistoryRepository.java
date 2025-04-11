b328e37da0423ee6ad1d3d2adbee96696086d644
Francisco Sousa <franciscoeds1987@gmail.com>
2025-04-11 10:04:52 -0300

    feat: criar repositório de histórico de alterações (ConsentHistory)

---
+++ src/main/java/com/sensedia/sample/consents/repository/ConsentHistoryRepository.java
@@ -0,0 +1,11 @@
+package com.sensedia.sample.consents.repository;
+
+import com.sensedia.sample.consents.domain.ConsentHistory;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ConsentHistoryRepository extends MongoRepository<ConsentHistory, UUID> {
+}
